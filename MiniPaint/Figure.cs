3192591e22abd57639e7163537adb5c1eb0b3a65
DzianisL <gestaltldi@yandex.ru>
2016-05-25 16:26:33 +0300

    trying to put first project on github

---
+++ MiniPaint/Figure.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+
+namespace _2_oop
+{
+    public abstract class Figure
+    {
+        private int _x;
+        private int _y;
+        public int x
+        {
+            get
+            {
+                return this._x;
+            }
+            protected set
+            {                
+                if (value <= 0)
+                    throw new Exception("Invalid parameter: less than zero!");
+                this._x = value;
+            }
+        }
+        public int y
+        {
+            get
+            {
+                return this._y;
+            }
+            protected set
+            {
+                if (value <= 0)
+                    throw new Exception("Invalid parameter: less than zero!");
+                this._y = value;
+            }
+        }
+        protected Figure(int _x, int _y)
+        {
+            x = _x;
+            y = _y;
+        }             
+    }
+    public interface Painter<T> where T: class
+    {
+        void Draw(T t);
+    }
+}
--- MiniPaint/PainterImplementations.cs
+++ MiniPaint/PainterImplementations.cs
@@ -7,75 +7,48 @@ using System.Drawing;
 
 namespace _2_oop
 {
-    class RectangleBuider : Painter //fabric of classes
+    class RectangleBuider : Painter<Rectangle> //fabric of classes
     {
         public void Draw(Rectangle rect)
         {
             Form1.g.FillRectangle(Form1.sb, rect.x, rect.y, rect.a, rect.b);
-        }
-        void Painter.Draw()
-        {
-
-        }
+        }        
     }
-    class CircleBuilder : Painter //fabric of classes
+    class CircleBuilder : Painter<Circle>  //fabric of classes
     {
         public void Draw(Circle circle)
         {
             Form1.g.FillEllipse(Form1.sb, circle.x, circle.y, circle.R, circle.R);
-        }
-        void Painter.Draw()
-        {
-
-        }
+        }       
     }
-    class EllipseBuilder : Painter //fabric of classes
+    class EllipseBuilder : Painter<Ellipse> //fabric of classes
     {
         public void Draw(Ellipse ellipse)
         {
             Form1.g.FillEllipse(Form1.sb, ellipse.x, ellipse.y, ellipse.a, ellipse.b);
         }
-        void Painter.Draw()
-        {
-
-        }
     }
-    class DotBuider : Painter //fabric of classes
+    class DotBuider : Painter<Dot> //fabric of classes
     {
         public void Draw(Dot dot)
         {
             Form1.g.FillRectangle(Form1.sb, dot.x, dot.y,3,3);
-        }
-
-        void Painter.Draw()
-        {
-
-        }
+        }        
     }
-    class LineBuilder : Painter //fabric of classes
+    class LineBuilder : Painter<Line> //fabric of classes
     {
         public void Draw(Line line)
         {
             Form1.g.DrawLine(new Pen(Form1.sb,2), new Point(line.x, line.y), new Point (line.x2, line.y2) );
         }
-
-        void Painter.Draw()
-        {
-
-        }
     }
-    class TriangleBuilder : Painter
+    class TriangleBuilder : Painter<Triangle>
     {
         public void Draw(Triangle triangle)
         {
             Form1.g.DrawLine(new Pen(Form1.sb,2), new Point(triangle.x, triangle.y), new Point(triangle.x2, triangle.y2));
             Form1.g.DrawLine(new Pen(Form1.sb, 2), new Point(triangle.x2, triangle.y2), new Point(triangle.x3, triangle.y3));
             Form1.g.DrawLine(new Pen(Form1.sb, 2), new Point(triangle.x3, triangle.y3), new Point(triangle.x, triangle.y));
-        }
-
-        void Painter.Draw()
-        {
-
         }
     }
 }
